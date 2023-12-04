6b3d7c405c60e8480180fededc806651e2b577bd
SLCKoenBT <koen.bouckhout@skyline.be>
2023-12-05 00:00:01 +0100

    Added some comments

--- Hello World_1/Hello World_1.cs
+++ Hello World_1/Hello World_1.cs
@@ -17,6 +17,7 @@ namespace Hello_World_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
+			//This generates an information event
 			engine.GenerateInformation("Hello World!");
 		}
 	}
